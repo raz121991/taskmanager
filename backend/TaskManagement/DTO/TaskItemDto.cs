72dec7aa56f889d1ec8cc92f69427e232bce8504
unknown <raz121991@walla.com>
2024-08-04 14:25:17 +0300

    Initial commit with frontend and backend

---
+++ backend/TaskManagement/DTO/TaskItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.DTO
+{
+    public class TaskItemDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
